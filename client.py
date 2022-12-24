65e25fe0328b312a555187da3e239008b9c21a8b
Elton Vivot <eltonvivot@gmail.com>
2022-12-23 21:48:04 -0300

    added sort, clear, index_of and remove methods for server

--- client.py
+++ client.py
@@ -3,7 +3,26 @@ from constRPYC import * #-
 
 class Client:
   conn = rpyc.connect(SERVER, PORT) # Connect to the server
-  print (conn.root.exposed_value())
+  print(f"values = {conn.root.exposed_value()}")
+  print("adding 5")
   conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
-  print (conn.root.exposed_value())   # Print the result
+  print("adding 6")                 # and append two elements
+  conn.root.exposed_append(6)
+  print(f"values = {conn.root.exposed_value()}")     # Print the result
+  print ("removing 5")
+  conn.root.exposed_remove(5)
+  print(f"values = {conn.root.exposed_value()}")     # Print the result
+  print(f"searching for '5'")
+  print(f"index of '5': {conn.root.exposed_index_of(5)}")
+  print(f"searching for '6'")
+  print(f"index of '6': {conn.root.exposed_index_of(6)}")
+  print("adding 3")                 # and append two elements
+  conn.root.exposed_append(3)
+  print(f"values = {conn.root.exposed_value()}")     # Print the result
+  print(f"sort values")
+  conn.root.exposed_sort()
+  print(f"values = {conn.root.exposed_value()}")     # Print the result
+  print(f"clear values")
+  conn.root.exposed_clear()
+  print(f"values = {conn.root.exposed_value()}")     # Print the result
+
--- server.py
+++ server.py
@@ -12,6 +12,20 @@ class DBList(rpyc.Service):
   def exposed_value(self):
     return self.value
 
+  def exposed_sort(self):
+    return self.value.sort()
+  
+  def exposed_clear(self):
+    return self.value.clear()
+  
+  def exposed_index_of(self, data):
+    return None if not data in self.value else self.value.index(data)
+  
+  def exposed_remove(self, data):
+    if not data in self.value: return f"'{data}' not in values"
+    self.value.remove(data)
+    return self.value
+
 if __name__ == "__main__":
   server = ThreadedServer(DBList(), port = PORT)
   server.start()
